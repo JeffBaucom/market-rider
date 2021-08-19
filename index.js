a662b607e3a636ae00db57b573698c2775f14c40
Jeffrey Baucom <kishi@Jeffreys-MacBook-Pro.local>
2021-08-19 17:12:02 -0400

    add client serving and initialize DB

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const winston = require('winston'),
+    expressWinston = require('express-winston');
+// const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const dbUrl = 'mongodb://localhost/waxy-bird';
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// app.use(cors({
+//     origin: '*',
+// }));
+
+// app.use(expressWinston.logger({
+//   transports: [
+//     new winston.transports.Console()
+//   ],
+//   format: winston.format.combine(
+//     winston.format.colorize(),
+//     winston.format.json()
+//   ),
+//   meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+//   msg: 'HTTP {{req.method}} {{req.url}}', // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+//   expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+//   colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+//   ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+// }));
+
+app.use(express.static(__dirname + 'client/public'));
+
+// add middlewares
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("client"));
+
+mongoose.connect(dbUrl);
+
+/* 
+* -------------
+* ROUTING -----  
+* ------------- 
+*/ 
+
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'hello from server!'});
+});
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
